23601d4f2db43e2c7282b7404f461fd4f4d16c9d
Lester Ong <lesterong776@gmail.com>
2025-01-24 12:06:39 +0800

    Initialise project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lester Ong on 24/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
