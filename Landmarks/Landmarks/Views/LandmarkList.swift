a59c6a1c62fc10ee36fe4c7ec108c5393892088e
Lester Ong <lesterong776@gmail.com>
2025-01-25 01:01:15 +0800

    Complete 2.4

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Lester Ong on 25/1/25.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
